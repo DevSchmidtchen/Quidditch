a1c0a0cffc192d38d0a0bf0f6cbd9ca89905dcfd
Matti Schmidt <Schmidt_Mat@GYMTSDOM.LOCAL>
2017-03-14 12:49:22 +0100

    ended lesson 2

---
+++ Quidditch/GameMember.java
@@ -0,0 +1,18 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * this class is used to store shared information about the members of the game
+ * 
+ * @author Matti Schmidt 
+ * @version 0.1
+ */
+public class GameMember extends Actor
+{
+    /**
+     * this class is used to store shared information about the members of the game
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
---
+++ Quidditch/Harry.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * object of the character of the game
+ * 
+ * @author Matti Schmidt 
+ * @version 0.1
+ */
+public class Harry extends GameMember
+{
+    
+    private static final GreenfootImage HARRY_FRONT = new GreenfootImage("HarryPotter Front.png");
+    
+    public Harry() {
+        setImage(HARRY_FRONT);
+    }
+    
+    /**
+     * Act - do whatever the Harry wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
